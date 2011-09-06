078c842ec5b4fa8f3dce980ffcb367afb5beb727
Declan Haigh <declan@declanhaigh.com>
2011-09-06 16:55:28 +1000

    Added heaps of comments. The answer link callback is more lightweight with the massive chain now in nextQuestion. Uses xpert.cleanTree.

--- scripts.js
+++ scripts.js
@@ -2,13 +2,19 @@
 
 	function addQuestion ( tree ) {
 
+		// get info about the current tree
 		var questionInfo = xpert.getQuestionInfo( tree ),
 
+			// the next question heading
 			$question  = $( "<h2/>" ).html( questionInfo.question ),
+
+			// container for the question (plus answers)
 			$container = $( "<div/>" ).hide().append( $question );
 
 		$.each( questionInfo.responses, function (i, answer) {
 
+			// the tree of each question - can
+			// be another tree or an answer
 			var next = questionInfo.subTree[ i ][ 1 ],
 
 			$answer = $( "<a/>", {
@@ -17,27 +23,23 @@
 				"html": answer
 			}).bind( "click", function () {
 
-				nextQuestion( next );
+				nextQuestion.call( this, next );
 
-				$( this ).animate( {"font-size": "1.5em"}, 200 )
-						 .addClass( "active" )
-						 .siblings( "a" ).addClass( "inactive" )
-						 .add( this )
-						   .removeClass( "radio" )
-						   .removeAttr( "href" )
-						   .unbind( "click" );
-
+				// don't follow the #
 				return false;
 
 			});
 
+			// show the answer
 			$container.append( $answer );
 
 		});
 
+		// show the question its answers
 		$( "#questions" ).append( $container );
 		$container.fadeIn();
 
+		// return the question element for scrolling
 		return $question;
 
 	}
@@ -48,15 +50,17 @@
 		result = result.split( "|" );
 
 		var language = result[ 0 ],
+			// wiki page of the language found
 			URL = "http://en.wikipedia.org/wiki/" + result[ 1 ],
 
 			message = "It's " + language + "!",
 
-			$result = $( "<h2/>" ).addClass( "result" ).hide().html( message ),
+			$result = $( "<h2/>" ).hide().addClass( "result" ).html( message ),
 
 			// set frameborder to 0 for IE
-			$wiki = $( "<iframe/>", {"src":  URL, "frameborder": 0} ).hide();
+			$wiki = $( "<iframe/>", {src:  URL, frameborder: 0} ).hide();
 
+		// add the result and the wiki iframe
 		$( "#questions" ).append( $result )
 						 .append( $wiki );
 
@@ -70,8 +74,9 @@
 
 	}
 
+	// animate the window's scroll position
+	// to the specified element
 	function scrollTo ( $el ) {
-
 		// do this animation then skip to
 		// the latest one
 		$( "body" ).stop().animate({
@@ -79,9 +84,11 @@
 			scrollTop: $el.offset().top
 
 		}, 1000 );
-
 	}
 
+	// insert the appropriate heading and scroll to it
+	// next can be an array of the next
+	// question(s) or the answer
 	function nextQuestion ( next ) {
 
 		var nextHeading = typeof next === "string" ?
@@ -89,13 +96,31 @@
 
 		scrollTo( nextHeading );
 
+		// "this" is the link of the answer chosen
+
+		$( this ).animate( {"font-size": "1.5em"}, 200 )
+				 .addClass( "active" )
+
+				 // disable other answers
+				 .siblings( "a" ).addClass( "inactive" )
+				 
+				  // disable all answers
+				 .add( this )
+				   .removeClass( "radio" )
+				   .removeAttr( "href" )
+				   .unbind( "click" );
+
 	}
 
 	// assume initial indent is 0, this
 	// strips out all whitespace before
 	// the first question
-	// TODO: trim() doesn't work on IE
-	var tree = $( "#tree" ).html().split( "\n" );
+	var tree = $("#tree").html();
+
+	// clean blank lines
+	tree = xpert.cleanTree( tree );
+
+	// make nested
 	tree = xpert( tree );
 
 	// if executed with the context of
@@ -108,13 +133,13 @@
 			$( this ).hide();
 		}
 
-		nextQuestion( tree );
+		addQuestion( tree );
 		return false;
 
 	}
 
-	init();
-
 	$( "#restart" ).click( init );
+
+	init();
 
 }() );
